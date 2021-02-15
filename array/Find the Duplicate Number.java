de8a55090047819265871dca601fd8e0d89c018b
Subham Kumar <69760328+subham500071430@users.noreply.github.com>
2021-02-15 13:43:21 +0530

    Update Find the Duplicate Number.java

--- array/Find the Duplicate Number.java
+++ array/Find the Duplicate Number.java
@@ -1,3 +1,4 @@
+//1st approach
 class Solution {
     public int findDuplicate(int[] nums) {
        Arrays.sort(nums);
@@ -5,6 +6,20 @@ class Solution {
            if(nums[i]==nums[i+1])
                return nums[i];
        }
+        return -1;
+    }
+}
+
+//2nd approach
+class Solution {
+    public int findDuplicate(int[] nums) {
+       HashSet<Integer> h=new HashSet<Integer>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(h.contains(nums[i]))
+                return nums[i];
+            h.add(nums[i]);
+        }
         return -1;
     }
 }
